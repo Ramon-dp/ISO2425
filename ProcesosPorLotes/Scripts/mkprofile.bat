7c7f5352231326b5849605626f800873e3f4489e
Ramon-dp <rdiezp02@iescastelar.com>
2024-11-18 09:56:14 +0100

    mkprofile.bat

--- ProcesosPorLotes/Scripts/mkprofile.bat
+++ ProcesosPorLotes/Scripts/mkprofile.bat
@@ -2,4 +2,14 @@
 REM Script de prueba
 REM Autor: Ramón Díez Parejo
 REM Fecha: 13-11-2024
-mkdir
+mkdir %USERPROFILE%\asir %USERPROFILE%\daw %USERPROFILE%\smr %USERPROFILE%\dam %USERPROFILE%\certi
+
+mkdir %USERPROFILE%\asir\docs %USERPROFILE%\asir\pract %USERPROFILE%\asir\exam
+
+mkdir  %USERPROFILE%\daw\docs %USERPROFILE%\daw\pract %USERPROFILE%\daw\exam
+
+mkdir %USERPROFILE%\smr\docs %USERPROFILE%\smr\pract %USERPROFILE%\smr\exam
+
+mkdir %USERPROFILE%\dam\docs %USERPROFILE%\dam\pract %USERPROFILE%\dam\exam
+
+mkdir %USERPROFILE%\certi\docs %USERPROFILE%\certi\pract %USERPROFILE%\certi\exam
