0ebf0e3ae5fc818605ab44e89e2ab1888f9eab2d
Ramon-dp <rdiezp02@iescastelar.com>
2024-11-19 09:16:09 +0100

    Add files via upload

---
+++ ProcesosPorLotes/Scripts/Script_media.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Script de Entregable
+REM Autor: Ramón Díez Parejo
+REM Fecha: 18-11-2024
+set /A suma= %1 + %2 + %3
+set /A media=(%1 + %2 + %3)/3
+echo La media de los numeros introducidos es %media%
